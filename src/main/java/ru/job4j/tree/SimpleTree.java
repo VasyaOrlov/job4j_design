4e4f1e1dee9535e679881ff2ee8cd19c42230022
VasiliiOrlov <ov_prog@mail.ru>
2022-04-26 10:02:05 +0300

    Добавить метод boolean isBinary() [#1712]

--- src/main/java/ru/job4j/tree/SimpleTree.java
+++ src/main/java/ru/job4j/tree/SimpleTree.java
@@ -1,6 +1,7 @@
 package ru.job4j.tree;
 
 import java.util.*;
+import java.util.function.Predicate;
 
 /**
  * класс реализует интерфейс Tree
@@ -29,19 +30,42 @@ public class SimpleTree<E> implements Tree<E> {
         return rsl;
     }
 
-    @Override
-    public Optional<Node<E>> findBy(E value) {
+    /**
+     * Метод находит узел на основании условия
+     * @param condition - условие
+     * @return Optional узел
+     */
+    private Optional<Node<E>> findByPredicate(Predicate<Node<E>> condition) {
         Optional<Node<E>> rsl = Optional.empty();
         Queue<Node<E>> data = new LinkedList<>();
         data.offer(this.root);
         while (!data.isEmpty()) {
             Node<E> el = data.poll();
-            if (el.value.equals(value)) {
+            if (condition.test(el)) {
                 rsl = Optional.of(el);
                 break;
             }
             data.addAll(el.children);
         }
         return rsl;
+    }
+
+    /**
+     * метод определяет является ли объект бинарным деревом
+     * @return  является ли объект бинарным деревом
+     */
+    @Override
+    public boolean isBinary() {
+        return findByPredicate(eNode -> eNode.children.size() > 2).isEmpty();
+    }
+
+    /**
+     * Метод находит узел со значением value
+     * @param value - значение ноды
+     * @return Optional узел
+     */
+    @Override
+    public Optional<Node<E>> findBy(E value) {
+        return findByPredicate(eNode -> eNode.value.equals(value));
     }
 }
--- src/test/java/ru/job4j/tree/SimpleTreeTest.java
+++ src/test/java/ru/job4j/tree/SimpleTreeTest.java
@@ -35,11 +35,24 @@ public class SimpleTreeTest {
     public void whenChildExistOnLeafThenNotAdd() {
         Tree<Integer> tree = new SimpleTree<>(1);
         tree.add(1, 2);
-        tree.add(2, 1);
+        tree.add(1, 3);
         tree.add(1, 4);
         tree.add(4, 5);
         tree.add(5, 6);
         assertFalse(tree.add(2, 6));
     }
 
+    @Test
+    public void binaryTest() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(2, 4);
+        tree.add(2, 5);
+        tree.add(3, 6);
+        tree.add(3, 7);
+        assertTrue(tree.isBinary());
+        tree.add(1, 8);
+        assertFalse(tree.isBinary());
+    }
 }
