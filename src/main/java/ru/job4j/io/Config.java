18ecada024ebc49644293a8a5c367748e0bf44d3
VasiliiOrlov <ov_prog@mail.ru>
2022-04-30 07:37:36 +0300

    Читаем файл конфигурации [#858]

---
+++ src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,73 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+
+/**
+ * класс считывает конфигурационный файл и добавляет значения в карту
+ */
+public class Config {
+
+    /**
+     * path - путь к файлу
+     * values - карта
+     */
+    private final String path;
+    private final Map<String, String> values = new HashMap<String, String>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    /**
+     * метод считывает конфигурационный файл и добавляет ключ-значение в карту
+     */
+    public void load() {
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines()
+                    .filter(e -> e.length() > 0)
+                    .filter(e -> !e.startsWith("#"))
+                    .forEach(e -> {
+                        if (!e.contains("=") || "=".equals(e)) {
+                            throw new IllegalArgumentException();
+                        }
+                        String[] word = e.split("=", 2);
+                        if (word[0].length() == 0 || word[1].length() == 0) {
+                            throw new IllegalArgumentException();
+                        } else {
+                            values.put(word[0], word[1]);
+                        }
+                    });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * метод возвращает значение карты по ключу
+     * @param key - ключ
+     * @return - значение в карте соответствующее данному ключу
+     */
+    public String value(String key) {
+        return values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Config("app.properties"));
+    }
+}
---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,69 @@
+package ru.job4j.io;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ConfigTest {
+
+    @Test
+    public void whenPairWithoutComment() {
+        String path = "./data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        System.out.println(config);
+        assertThat(config.value("name"), is("Vasia"));
+        assertThat(config.value("surname"), is(Matchers.nullValue()));
+    }
+
+    @Test
+    public void whenEmpty_lines_and_comment() {
+        String path = "./data/empty_lines_and_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        System.out.println(config);
+        assertThat(config.value("colour"), is("orange"));
+        assertThat(config.value("length"), is("five"));
+        assertThat(config.value("surname"), is(Matchers.nullValue()));
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void whenViolation1() {
+        String path = "./data/violation1.properties";
+        Config config = new Config(path);
+        config.load();
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void whenViolation2() {
+        String path = "./data/violation2.properties";
+        Config config = new Config(path);
+        config.load();
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void whenViolation3() {
+        String path = "./data/violation3.properties";
+        Config config = new Config(path);
+        config.load();
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void whenViolation4() {
+        String path = "./data/violation4.properties";
+        Config config = new Config(path);
+        config.load();
+    }
+
+    @Test
+    public void whenViolation5() {
+        String path = "./data/violation5.properties";
+        Config config = new Config(path);
+        config.load();
+        System.out.println(config);
+        assertThat(config.value("key"), is("value=1"));
+        assertThat(config.value("signal"), is("red="));
+        assertThat(config.value("surname"), is(Matchers.nullValue()));
+    }
+}
