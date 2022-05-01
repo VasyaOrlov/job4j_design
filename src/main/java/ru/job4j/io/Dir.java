dc37d028eccf536b15484d45126d71d461e9fbd2
VasiliiOrlov <ov_prog@mail.ru>
2022-05-01 09:42:12 +0300

    File [#252491]

---
+++ src/main/java/ru/job4j/io/Dir.java
@@ -0,0 +1,22 @@
+package ru.job4j.io;
+
+import java.io.File;
+
+/**
+ * Класс демонстрирует работу с объектом типа File
+ */
+public class Dir {
+    public static void main(String[] args) {
+        File file = new File("D:\\java\\projects");
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+        System.out.printf("size : %s%n", file.getTotalSpace());
+        for (File subfile : file.listFiles()) {
+            System.out.printf("%s, %s%n", subfile.getName(), subfile.length());
+        }
+    }
+}
