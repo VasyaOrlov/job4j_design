eaa95200ef4a3bef0c5faf3fd0de34e31f17832f
VasiliiOrlov <ov_prog@mail.ru>
2022-05-02 19:18:46 +0300

    Анализ доступности сервера. [#859 #295550] fix2

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -36,11 +36,7 @@ public class Analizy {
     }
 
     public static void main(String[] args) {
-        try (PrintWriter out = new PrintWriter(new FileOutputStream("unavailable.csv"))) {
-            out.println("15:01:30;15:02:32");
-            out.println("15:10:30;23:12:32");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        Analizy analizy = new Analizy();
+        analizy.unavailable("./data/test.txt", "./data/rsl.txt");
     }
 }
