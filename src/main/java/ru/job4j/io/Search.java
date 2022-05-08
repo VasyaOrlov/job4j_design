b5db52a388e93afbab3c003a2609ecd98549fb86
VasiliiOrlov <ov_prog@mail.ru>
2022-05-08 17:39:46 +0300

    Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -7,15 +7,43 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.function.Predicate;
 
+/**
+ * класс реализует поиск файлов по определенному условию
+ */
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith(".js")).forEach(System.out::println);
+        if (validate(args)) {
+            Path start = Paths.get(args[0]);
+            search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
+        }
     }
 
+    /**
+     * метод проходит по директории указанного пути и возвращает список path удовлетворяющих условию
+     * @param root - путь
+     * @param condition - условие
+     * @return - список path
+     * @throws IOException - IO сключение
+     */
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    /**
+     * метод проводит валидацию входных параметров
+     * @param args - массив аргументов
+     * @return - валидация
+     */
+    public static boolean validate(String[] args) {
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Заполните параметры запуска.");
+        } else if (!Files.exists(Paths.get(args[0]))) {
+            throw new IllegalArgumentException("Некорректный путь");
+        } else if (!args[1].startsWith(".")) {
+            throw new IllegalArgumentException("Некорректное разрешение файла");
+        }
+        return true;
     }
 }
