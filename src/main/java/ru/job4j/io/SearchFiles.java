c682d6728497aa265dcee54b29986678e831444a
VasiliiOrlov <ov_prog@mail.ru>
2022-05-10 02:09:25 +0300

    Архивировать проект [#861 #299041] fix1

--- src/main/java/ru/job4j/io/SearchFiles.java
+++ src/main/java/ru/job4j/io/SearchFiles.java
@@ -28,7 +28,7 @@ public class SearchFiles implements FileVisitor<Path> {
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
         if (condition.test(file)) {
-            list.add(file.toAbsolutePath());
+            list.add(file);
         }
         return CONTINUE;
     }
--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -9,7 +9,6 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Zip {
-
     public void packFiles(List<Path> sources, File target) {
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
             for (Path temp : sources) {
@@ -23,30 +22,21 @@ public class Zip {
         }
     }
 
-    public void packSingleFile(File source, File target) {
-        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
-            zip.putNextEntry(new ZipEntry(source.getPath()));
-            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
-                zip.write(out.readAllBytes());
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+    private void validate(String[] args) {
+        ArgsName arg = ArgsName.of(args);
+        if (!new File(arg.get("d")).exists()) {
+            throw new IllegalArgumentException("Директория не существует");
         }
+        arg.get("e");
+        arg.get("o");
     }
 
     public static void main(String[] args) throws IOException {
         Zip zip = new Zip();
-        zip.packSingleFile(
-                new File("./pom.xml"),
-                new File("./pom.zip")
-        );
+        zip.validate(args);
         ArgsName arg = ArgsName.of(args);
         String directory = arg.get("d");
-        if (!new File(directory).exists()) {
-            throw new IllegalArgumentException("Директория не существует");
-        }
         String exclude = arg.get("e").substring(1);
-        System.out.println(exclude);
         String output = arg.get("o");
         Predicate<Path> predicate = p -> !p.getFileName().toString().endsWith(exclude);
         List<Path> list = Search.search(Paths.get(directory), predicate);
