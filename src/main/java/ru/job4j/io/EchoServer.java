6656bdab15f406e55924daeeb19fd5eb8976fdd4
VasiliiOrlov <ov_prog@mail.ru>
2022-05-14 13:27:09 +0300

    Бот [#7921 #300788] fix1

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -14,9 +14,13 @@ public class EchoServer {
                              new InputStreamReader(socket.getInputStream()))) {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     String str = in.readLine();
-                        if (str.contains("/?msg=Bye")) {
-                            server.close();
-                        }
+                    if (str.contains("/?msg=Exit")) {
+                        server.close();
+                    } else if (str.contains("/?msg=Hello")) {
+                        out.write("Hello".getBytes());
+                    } else if (str.contains("/?msg")) {
+                        out.write(("What").getBytes());
+                    }
                     out.flush();
                 }
             }
