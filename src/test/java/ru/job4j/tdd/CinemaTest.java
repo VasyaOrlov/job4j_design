e8a032a278711f81f277894064c62286897f6b16
VasiliiOrlov <ov_prog@mail.ru>
2022-06-13 08:57:06 +0300

    Что такое TDD? [#4918] fix1

--- src/test/java/ru/job4j/tdd/CinemaTest.java
+++ src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -5,8 +5,6 @@ import static org.hamcrest.Matchers.is;
 
 import org.junit.Ignore;
 import org.junit.Test;
-
-import java.util.Arrays;
 import java.util.Calendar;
 import java.util.List;
 
@@ -18,14 +16,14 @@ public class CinemaTest {
         Account account = new AccountCinema();
         Cinema cinema = new Cinema3D();
         Calendar date = Calendar.getInstance();
-        date.set(2020, 10, 10, 23, 00);
+        date.set(2022, Calendar.AUGUST, 10, 23, 0);
         Ticket ticket = cinema.buy(account, 1, 1, date);
         assertThat(ticket, is(new Ticket3D()));
     }
 
     @Test
     public void addSession() {
-        Cinema cinema = new Cinema3D();
+        Cinema3D cinema = new Cinema3D();
         cinema.add(new Session3D());
         cinema.add(new Session4D());
         assertThat(cinema.getListSession(), is(List.of(new Session3D(), new Session4D())));
@@ -36,6 +34,24 @@ public class CinemaTest {
         Cinema cinema = new Cinema3D();
         cinema.add(new Session3D());
         List<Session> sessions = cinema.find(session -> true);
-        assertThat(sessions, is(Arrays.asList(new Session3D())));
+        assertThat(sessions, is(List.of(new Session3D())));
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void whenIncorrectDate() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2005, Calendar.NOVEMBER, 10, 23, 10);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void whenIncorrectPlace() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2022, Calendar.AUGUST, 10, 23, 0);
+        Ticket ticket = cinema.buy(account, -1, 500, date);
     }
 }
