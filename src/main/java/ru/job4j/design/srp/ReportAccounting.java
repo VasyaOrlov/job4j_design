ca4eed4799634bd4de5ea0656e5ab15da862ef30
VasiliiOrlov <ov_prog@mail.ru>
2022-06-20 16:30:02 +0300

    Отчеты. [#850] fix1

--- src/main/java/ru/job4j/design/srp/ReportAccounting.java
+++ src/main/java/ru/job4j/design/srp/ReportAccounting.java
@@ -5,6 +5,8 @@ import java.util.function.Predicate;
 
 public class ReportAccounting implements Report {
 
+    private static final int DOLLAR = 60;
+
     public static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd:MM:yyyy HH:mm");
 
     private final Store store;
@@ -19,7 +21,7 @@ public class ReportAccounting implements Report {
         text.append("Name; Hired; Fired; Salary($);")
                 .append(System.lineSeparator());
         for (Employee employee : store.findBy(filter)) {
-            double salary = employee.getSalary() / 60;
+            double salary = employee.getSalary() / DOLLAR;
             text.append(employee.getName()).append(";")
                 .append(DATE_FORMAT.format(employee.getHired().getTime())).append(";")
                 .append(DATE_FORMAT.format(employee.getFired().getTime())).append(";")
